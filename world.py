42b72eb4d285e63c0291e03dd793435ce8c9fc5a
Matt Hagner <web@matthagner.com>
2020-05-03 13:10:46 -0500

    Fix linting issues with world.py

--- world.py
+++ world.py
@@ -1,21 +1,32 @@
+"""
+Used to generate a world from a map file
+"""
+
 from room import Room
 import random
 import math
 
 class World:
+    """
+    The world class stores the state of the world when loaded from a given map
+    """
     def __init__(self):
         self.starting_room = None
         self.rooms = {}
         self.room_grid = []
         self.grid_size = 0
+
     def load_graph(self, room_graph):
         num_rooms = len(room_graph)
-        rooms = [None] * num_rooms
+        # rooms = [None] * num_rooms
         grid_size = 1
         for i in range(0, num_rooms):
-            x = room_graph[i][0][0]
+            # x = room_graph[i][0][0]
             grid_size = max(grid_size, room_graph[i][0][0], room_graph[i][0][1])
-            self.rooms[i] = Room(f"Room {i}", f"({room_graph[i][0][0]},{room_graph[i][0][1]})",i, room_graph[i][0][0], room_graph[i][0][1])
+            coords = (room_graph[i][0][0], room_graph[i][0][1])
+            self.rooms[i] = Room(f"Room {i}",
+                                 f"({room_graph[i][0][0]},{room_graph[i][0][1]})", i,
+                                 coords)
         self.room_grid = []
         grid_size += 1
         self.grid_size = grid_size
@@ -42,7 +53,7 @@ class World:
             for j in range(len(self.room_grid[0])):
                 rotated_room_grid[len(self.room_grid[0]) - j - 1][i] = self.room_grid[i][j]
         print("#####")
-        str = ""
+        output = ""
         for row in rotated_room_grid:
             all_null = True
             for room in row:
@@ -52,38 +63,37 @@ class World:
             if all_null:
                 continue
             # PRINT NORTH CONNECTION ROW
-            str += "#"
+            output += "#"
             for room in row:
-                if room is not None and room.n_to is not None:
-                    str += "  |  "
+                if room is not None and room.get_room_in_direction('n') is not None:
+                    output += "  |  "
                 else:
-                    str += "     "
-            str += "#\n"
+                    output += "     "
+            output += "#\n"
             # PRINT ROOM ROW
-            str += "#"
+            output += "#"
             for room in row:
-                if room is not None and room.w_to is not None:
-                    str += "-"
+                if room is not None and room.get_room_in_direction('w') is not None:
+                    output += "-"
                 else:
-                    str += " "
+                    output += " "
                 if room is not None:
-                    str += f"{room.id}".zfill(3)
+                    output += f"{room.id}".zfill(3)
                 else:
-                    str += "   "
-                if room is not None and room.e_to is not None:
-                    str += "-"
+                    output += "   "
+                if room is not None and room.get_room_in_direction('e') is not None:
+                    output += "-"
                 else:
-                    str += " "
-            str += "#\n"
+                    output += " "
+            output += "#\n"
             # PRINT SOUTH CONNECTION ROW
-            str += "#"
+            output += "#"
             for room in row:
-                if room is not None and room.s_to is not None:
-                    str += "  |  "
+                if room is not None and room.get_room_in_direction('s') is not None:
+                    output += "  |  "
                 else:
-                    str += "     "
-            str += "#\n"
-        print(str)
+                    output += "     "
+            output += "#\n"
+        print(output)
         print("#####")
-
 
